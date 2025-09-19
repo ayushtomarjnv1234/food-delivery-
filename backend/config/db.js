fa613fdd4c0094ec5862cff8e6098b8acd77184e
Ayush Tomar <ayushtomarjnv1234@gmail.com>
2025-09-20 01:09:48 +0530

    Add start script and update DB config

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,10 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();  // Load .env file
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://ayushtomar:8077898954@cluster0.zj3zxcx.mongodb.net/food-del').then(()=>console.log("DB Connected"));
+    await mongoose.connect(process.env.MONGO_URI)
+        .then(() => console.log("DB Connected"))
+        .catch((err) => console.log("DB Connection Error:", err));
 }
