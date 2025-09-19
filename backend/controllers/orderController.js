2eec80fa0bfee65fa55c903770179505395b9c6e
Ayush Tomar <ayushtomarjnv1234@gmail.com>
2025-09-20 02:21:12 +0530

    frontend url in backend is updated as deployed

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -8,7 +8,7 @@ console.log(process.env.STRIPE_SECRET_KEY);
 //placing user order for frontend
 const placeOrder = async (req,res) => {
 
-    const frontend_url = "http://localhost:5173"
+    const frontend_url = "https://food-del-frontend-app.vercel.app/"
 
     try {
         const newOrder = new orderModel({
