f8423eb23c54d31432167596109e996aa5618f05
Ayush Tomar <ayushtomarjnv1234@gmail.com>
2025-11-23 00:56:32 +0530

    Force hardcoded database name to ignore env vars

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    const mongoUri = process.env.MONGODB_URI || 'mongodb+srv://ayush:ayush123%40@cluster0.cnkcimb.mongodb.net/food-delivery-fresh';
-    await mongoose.connect(mongoUri).then(()=>console.log("DB Connected to:", mongoUri.split('@')[1].split('/')[0]));
+    // Hardcoded to force new database - ignore environment variables
+    const mongoUri = 'mongodb+srv://ayush:ayush123%40@cluster0.cnkcimb.mongodb.net/food-delivery-fresh-v2';
+    await mongoose.connect(mongoUri).then(()=>console.log("DB Connected to:", mongoUri.split('@')[1]));
 }
